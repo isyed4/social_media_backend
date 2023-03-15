4972f245c8e2032ea29690414e88aa46021638ae
Imran Syed <imran@Imrans-MacBook-Air.local>
2023-03-14 22:42:14 -0500

    server is up and running

--- index.js
+++ index.js
@@ -39,4 +39,11 @@ const upload = multer({storage})
 // mongoose setup
 
 const PORT = process.env.PORT || 6001;
-mongoose.connect
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server Port: ${PORT}`);
+    })
+}).catch((error)=> console.log(`${error} did not connect`))
